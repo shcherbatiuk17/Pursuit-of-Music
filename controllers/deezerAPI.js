c86e111407f6c48319c2a27e3bc5fdad700533e4
Marian <shcherbatiuk.m17@gmail.com>
2023-08-15 20:38:22 -0400

    Added a new file: controllers/deezerAPI.js - Modified: controllers/index.js - Added a new file: package-lock.json - Modified: server.js

---
+++ controllers/deezerAPI.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+function getTopSongsByArtist(artistName, limit = 5) {
+    const searchUrl = `https://api.deezer.com/search/artist?q=${encodeURIComponent(artistName)}&limit=1&output=json`;
+  
+    return fetch(searchUrl)
+      .then(response => response.json())
+      .then(data => {
+        const artistId = data.data[0]?.id;
+        if (artistId) {
+          const apiUrl = `https://api.deezer.com/artist/${artistId}/top?limit=${limit}&output=json`;
+          return fetch(apiUrl);
+        } else {
+          throw new Error('Artist not found.');
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        const songs = data.data;
+        const songInfoPromises = songs.map(song => {
+          const trackUrl = `https://api.deezer.com/track/${song.id}`;
+          return fetch(trackUrl).then(response => response.json());
+        });
+  
+        return Promise.all(songInfoPromises);
+      })
+      .then(songsInfo => {
+        const songData = songsInfo.map(songInfo => {
+          return {
+            title: songInfo.title,
+            coverUrl: songInfo.album.cover_medium
+          };
+        });
+        return songData;
+      })
+      .catch(error => {
+        throw new Error(`Fetch error: ${error}`);
+      });
+  }
+  module.exports = {
+    getTopSongsByArtist
+  };
+  
+  
